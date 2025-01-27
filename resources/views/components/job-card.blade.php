1ed85833c448015a6d08f0fd9c9e4c0927136b3c
koeuk <koeukkos@gmail.com>
2025-01-27 23:59:22 +0700

    add initial layout and components; update dependencies and integrate Ziggy for route management

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <section>
+        <h3 class="mb-6">Job Title</h3>
+        <div class="px-4 bg-white/5 rounded-xl flex flex-col text-center">
+            <div class="seft-start text-sm">Laracast</div>
+            <diz class="py-8 font-bold">
+                <h3>Video Producer</h3>
+                <p>Full Time from 200$</p>
+            </diz>
+            <div class="flex justify-between text-center mt-auto">
+                <div >
+                    <x-tag>Tags</x-tag>
+                    <x-tag>Tags</x-tag>
+                    <x-tag>Tags</x-tag>
+                </div>
+                <img src="http:://placehold.it/42/42" alt="" class="rounded-xl">
+            </div>
+        </div>
+    </section>
+</x-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,28 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'primary': '#3490dc',
+                'secondary': '#2ecc71',
+                'success': '#2ecc71',
+                info: '#3490dc',
+                warning: '#f1c40f',
+                danger: '#e74c3c',
+                light: '#ecf0f1',
+                dark: '#34495e',
+                'black': '#000000',
+                gray: {
+                    100: '#f8f9fa',
+                    200: '#e9ecef',
+                    300: '#dee2e6',
+                    400: '#ced4da',
+                    500: '#b0bec5',
+                    600: '#757575',
+                    700: '#495057',
+                    800: '#343a40',
+                    900: '#212121',
+                },
+            }
         },
     },
     plugins: [],
